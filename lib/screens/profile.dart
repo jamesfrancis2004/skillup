5b24762533603df74cbfa8e921206a7dc8404c22
James Hocking <jameshocking542@gmail.com>
2024-08-24 06:25:16 +1000

    got warning about LF to CRLF, starting on profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 
+// pull the user from the database
+
+
+//
+
+
 // WIDGET 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});  
@@ -12,18 +18,32 @@ class ProfilePage extends StatefulWidget {
 
 // STATE
 class _ProfilePageState extends State<ProfilePage> {
-  @override
+  @ override
   Widget build(BuildContext context) {
-    return Center(
-      child: ColoredBox(
-        color: Colors.red,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('EXPLORE', style: Theme.of(context).textTheme.headlineMedium)
-          ],
-        )
-      )
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              double screenWidth = constraints.maxWidth;
+              double screenHeight = constraints.maxHeight;
+
+              return Container(
+                width: screenWidth * 0.8, // 50% of the screen width
+                height: screenHeight * 0.3, // 30% of the screen height
+                color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    'Username',
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
