b97f2417503f24d024525e8770e09f6b7986fa36
James <jamespcfrancis@gmail.com>
2024-08-25 05:52:11 +1000

    Fixed mesages.

--- lib/screens/community.dart
+++ lib/screens/community.dart
@@ -470,7 +470,7 @@ class _ExplorePageState extends State<ExplorePage> {
             ),
           ),
           SizedBox(
-            height: MediaQuery.of(context).size.height / 100,
+            height: MediaQuery.of(context).size.height / 200,
           )
         ],
       ),
