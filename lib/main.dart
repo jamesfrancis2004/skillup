bb8f6e0b0a3cb496e0d207db88ff3ecb67d35fcb
Joshua Hallam <joshua.hallam127@gmail.com>
2024-08-24 17:30:10 +1000

    ya

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-      getUser('1dS0BtUZf0E1N4LmIznn').then((user) => {print(user?.name)});
+      getUser('1dS0BtUZf0E1N4LmIznn').then((user) => {print(user?.friends)});
     });
   }
 
