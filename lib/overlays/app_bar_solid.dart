dbce0f218539371fd146cc75d8c47e30c31b63d0
Joshua Hallam <joshua.hallam127@gmail.com>
2024-08-25 03:49:20 +1000

    notifications now changes the skill

--- lib/overlays/app_bar_solid.dart
+++ lib/overlays/app_bar_solid.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:skillup/config.dart';
+import 'package:skillup/functions/skills.dart';
 
-
-// CONFIG ... 
+// CONFIG ...
 
 const double _expandedheight = kToolbarHeight * 0.75;
 const double _gradientWidth = 1.0;
 const double _edgePadding = 20.0;
 const double _bottomPadding = 10.0;
-
 
 // APP BAR ..
 
@@ -28,9 +27,7 @@ class AppBarSolid extends StatefulWidget implements PreferredSizeWidget {
   Size get preferredSize => AppBarSolid.preferredHeight; // kToolbarHeight
 }
 
-
-class _AppBarState extends State<AppBarSolid>  {
-
+class _AppBarState extends State<AppBarSolid> {
   // void handleScroll() {
   //   setState(() {
   //     print('appbar scroll handler called');
@@ -39,63 +36,50 @@ class _AppBarState extends State<AppBarSolid>  {
 
   @override
   Widget build(BuildContext context) {
-
     // Using PreferredSize to control height of appbar
     return PreferredSize(
-      preferredSize: const Size.fromHeight(kToolbarHeight),
-      child: AppBar(
-        shadowColor: null,
-        surfaceTintColor: null,
-        scrolledUnderElevation: 0.0,
-        toolbarHeight: _expandedheight,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        automaticallyImplyLeading: false,
-        leadingWidth: 150,
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: AppBar(
+          shadowColor: null,
+          surfaceTintColor: null,
+          scrolledUnderElevation: 0.0,
+          toolbarHeight: _expandedheight,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          automaticallyImplyLeading: false,
+          leadingWidth: 150,
 
-        // The name on the left of the AppBar
-        leading: Padding(
-          padding: const EdgeInsets.only(left: _edgePadding, bottom: _bottomPadding),
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              'SkillUp',
-              softWrap: false,
-              style: Theme.of(context).textTheme.displayMedium
-            )
-          )
-        ),
+          // The name on the left of the AppBar
+          leading: Padding(
+              padding: const EdgeInsets.only(
+                  left: _edgePadding, bottom: _bottomPadding),
+              child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text('SkillUp',
+                      softWrap: false,
+                      style: Theme.of(context).textTheme.displayMedium))),
 
-        // The icon on the right of the app bar
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: _edgePadding, bottom: _bottomPadding),
-            child: GestureDetector(
-              onTap: () => {print('pressed appbar action')},
-              child: Icon(
-                Icons.notifications,
-                size: 32,
-                color: Theme.of(context).colorScheme.onPrimary
-              )
-            )
-          ),
-        ],
+          // The icon on the right of the app bar
+          actions: [
+            Padding(
+                padding: const EdgeInsets.only(
+                    right: _edgePadding, bottom: _bottomPadding),
+                child: InkWell(
+                    onTap: () => {CurrentSkill.updateSkill()},
+                    child: Icon(Icons.notifications,
+                        size: 32,
+                        color: Theme.of(context).colorScheme.onPrimary))),
+          ],
 
-        // Background of main appbar space
-        // flexibleSpace: Stack(fit: StackFit.expand, children: []),
+          // Background of main appbar space
+          // flexibleSpace: Stack(fit: StackFit.expand, children: []),
 
-        // Bottom border gradient
-        bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(_gradientWidth),
-            child: Container(
-              height: _gradientWidth,
-              decoration: const BoxDecoration(
-                  gradient: highlightGradient
-              ),
-            )
-        ),
-
-      )
-    );
+          // Bottom border gradient
+          bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(_gradientWidth),
+              child: Container(
+                height: _gradientWidth,
+                decoration: const BoxDecoration(gradient: highlightGradient),
+              )),
+        ));
   }
-
 }
